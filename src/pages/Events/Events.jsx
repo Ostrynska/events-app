967e4a2d2235ca5fdc5655497750c7359b5fa516
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-05-15 13:54:40 +0300

    Update Events.jsx

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -5,6 +5,7 @@ import { fetchEvents } from '../../redux/events/operations';
 import EventsList from '../../components/EventsList/EventsList';
 import Title from '../../components/Title/Title';
 import SortBar from '../../components/SortBar/SortBar';
+import ScrollToTop from '../../components/Buttons/ScrollToTop/ScrollToTop';
 
 const EventsPage = () => {
   const [sortedEvents, setSortedEvents] = useState([]);
@@ -14,16 +15,18 @@ const EventsPage = () => {
   const dispatch = useDispatch();
   const events = useSelector(getEvents);
   const loading = useSelector(getLoading);
+  const [visibleEvents, setVisibleEvents] = useState(events.slice(0, 9));
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
     dispatch(fetchEvents());
   }, [dispatch]);
 
   useEffect(() => {
-    if (events.length > 0) {
-      const sorted = [...events].sort((a, b) => {
-        const propA = a[sortBy] || ''; // Якщо властивість не існує, встановлюємо порожній рядок
-        const propB = b[sortBy] || ''; // Якщо властивість не існує, встановлюємо порожній рядок
+    if (visibleEvents.length > 0) {
+      const sorted = [...visibleEvents].sort((a, b) => {
+        const propA = a[sortBy] || '';
+        const propB = b[sortBy] || '';
         if (sortDirection === 'asc') {
           return propA.localeCompare(propB);
         } else {
@@ -32,11 +35,49 @@ const EventsPage = () => {
       });
       setSortedEvents(sorted);
     }
-  }, [events, sortBy, sortDirection]);
+  }, [visibleEvents, sortBy, sortDirection]);
 
   const handleSort = (criteria, direction) => {
     setSortBy(criteria);
     setSortDirection(direction);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      const maxScroll =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      setScrollPosition(position);
+      if (
+        position >= maxScroll &&
+        !loading &&
+        visibleEvents.length < events.length
+      ) {
+        loadMoreEvents();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [loading, visibleEvents, events]);
+
+  const loadMoreEvents = () => {
+    const remainingEvents = events.slice(visibleEvents.length);
+    const nextEvents = remainingEvents.slice(0, 6);
+    const updatedVisibleEvents = [...visibleEvents, ...nextEvents];
+    const sorted = [...updatedVisibleEvents].sort((a, b) => {
+      const propA = a[sortBy] || '';
+      const propB = b[sortBy] || '';
+      if (sortDirection === 'asc') {
+        return propA.localeCompare(propB);
+      } else {
+        return propB.localeCompare(propA);
+      }
+    });
+    setVisibleEvents(sorted);
   };
 
   return (
@@ -44,6 +85,7 @@ const EventsPage = () => {
       <Title text="Events" />
       <SortBar onSort={handleSort} />
       <EventsList events={sortedEvents} loading={loading} />
+      <ScrollToTop />
     </main>
   );
 };
